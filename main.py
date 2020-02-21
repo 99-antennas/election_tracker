761bf233c276fa5fc8d9bbac5b6698890a19e090
kstohr <kstohr@users.noreply.github.com>
2020-02-20 23:24:47 -0400

    updates utils to save temp files, updates main.py to call new utils.

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ def run_current_elections(event, context):
     Cloud function to run job to fetch election data from Google Civic Information API.
     Stores data in a cloud storage bucket as a json file.
     """
-    # Local testing Only 
+    # Local testing only 
     # GOOGLE_APPLICATION_CREDENTIALS = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-    TODAY = dt.datetime.now()
-
+    
+    date = dt.datetime.now()
+    client = CloudStorageClient()
+    
     # Job status
     logging.info("Starting job fetch elections.")
     logging.info("""Trigger: messageId {} published at {}""".format(context.event_id, context.timestamp))
@@ -29,30 +31,19 @@ def run_current_elections(event, context):
     data = get_elections.fetch_elections()
 
     # Store data in temp file
-    path = "/tmp/"
-    if not os.path.exists(path):
-        os.mkdir(path)
-    filepath = path + 'data.json'
-
     if data:
-        try:
-            with open(filepath, 'w', encoding='utf-8') as file:
-                json.dump(data, file, ensure_ascii=False, indent=4)
-            logging.info(f"Data saved to '{filepath}''")
-        except Exception as e:
-            logging.error(e)
-            raise
+        client.save_tmp_json('data.json', data)
+        logging.info(f"Data saved to '{filepath}''")
     else:
         logging.error("Error: No data returned.")
         return
 
     # Upload temp file to Google Gloud Storage
-    client = CloudStorageClient()
     bucket_name = "current_elections"
     # Store file as most current
     client.upload_file(filepath, bucket_name, blob_name='current_elections.json')
     # Store file by date
-    client.upload_file(filepath, bucket_name, blob_name=f'{TODAY}.json')
+    client.upload_file(filepath, bucket_name, blob_name=f'{date}.json')
 
     # Job status
     logging.info("Completed job fetch elections.")
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import sys
 import os
 import logging
 import json
+import datetime as dt
+
 from google.cloud import storage
 
 class CloudStorageClient():
@@ -13,31 +15,87 @@ class CloudStorageClient():
     Stores files on Google Cloud Storage
     """
     def __init__(self):
+        """
+        Opens Google Cloud Storage connection. 
+        Creates root /tmp/ directory.
+        """
         try: 
             self.client = storage.Client()
-            logging.info("Connected to Google Cloud Storage.")
+            logging.debug("Connected to Google Cloud Storage.")
         except Exception as error: 
             logging.error("Error connecting to Google Cloud Storage:")
             logging.error(error)
             
+        try: 
+            self.path = "/tmp/"
+            if not os.path.exists(self.path):
+                os.mkdir(self.path)
+        except Exception as error: 
+            logging.error(f"Failed to create dir {self.path}")
+            logging.error(error)
+            
     def upload_file(self, filepath, bucket_name, blob_name):
+        """
+        Uploads a files from a local directory to Google Cloud Storage. 
+        Takes: 
+        - filepath to the file stored locally 
+        - bucket name on Google Cloud Storage 
+        - blob_name on Google Cloud Storage
+        """
         try: 
             bucket = self.client.get_bucket(bucket_name)
             blob = bucket.blob(blob_name)
             with open(filepath, 'rb') as file:
                 blob.upload_from_file(file)
-            logging.info(f"Loaded file {filepath} to {blob_name}")
+            logging.debug(f"Successfully loaded file from {filepath} to {blob_name}")
         except Exception as error: 
             logging.error("Error storing the file to Google Cloud Storage:")
             logging.error(error)
     
     def download_file(self, filepath, bucket_name, blob_name):
+        """
+        Downloads a files from Google Cloud Storage to a local directory. 
+        
+        Takes: 
+        - filepath where the file should be stored locally 
+        - bucket name on Google Cloud Storage 
+        - blob_name on Google Cloud Storage
+        """
         try: 
             bucket = self.client.get_bucket(bucket_name)
             blob = bucket.blob(blob_name)
             with open(filepath, 'wb') as file:
                 blob.download_to_file(file)
-            logging.info(f"Loaded file {filepath} to {blob_name}")
+            logging.debug(f"Downloaded file from gs://{bucket_name}/{blob_name} to {filepath}")
         except Exception as error: 
-            logging.error("Error retreiving the file from Google Cloud Storage:")
+            logging.error("Error retreiving the file from Google Cloud Storage.")
+            logging.error(error)
+        
+    def save_tmp_json(self, filename, data):
+        """
+        Temporarily stores json as a local temp file to /tmp/.
+        """
+        try: 
+            logging.debug("Saving json... ")
+            filepath = os.path.join(self.path, filename)
+            with open(filepath, 'w', encoding='utf-8') as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
+            logging.debug(f"Successfully saved json to {filepath}.")
+        except Exception as error: 
+            logging.error(f'Failed to save json to {self.path}')
+            logging.error(error)
+    
+    def load_tmp_json(self, filename):
+        """
+        Retrieves json from local temp file in /tmp directory.  
+        """
+        try: 
+            logging.debug("Loading json... ")
+            filepath = os.path.join(self.path, filename)
+            with open(filepath, 'rb') as file:
+                data = json.load(file)
+            logging.debug(f"Successfully loaded json from {filepath}.")
+            return data
+        except Exception as error: 
+            logging.error(f'Failed to load json from {os.path.join(self.path, filename)}')
             logging.error(error)
