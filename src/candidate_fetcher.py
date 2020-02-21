714af94fe80130434abdb704d20a065e08f47fbb
kstohr <kstohr@users.noreply.github.com>
2020-02-21 00:01:14 -0400

    updates candidate_fetcher.py

--- src/candidate_fetcher.py
+++ src/candidate_fetcher.py
@@ -3,14 +3,17 @@
 # Copyright 2020 99 Antennas LLC 
 
 """
-Fetch candidates info from Google Civic Information API
+Fetch voter info from Google Civic Information API
 """
 import sys
 import os
 import logging
 import json
+import datetime as dt
+import pandas as pd
 import requests
 from google.cloud import storage
+from src.utils import CloudStorageClient
 
 
 class ReverseGeocode(): 
@@ -66,6 +69,11 @@ class VoterInfo():
     def __init__(self):
         self._url = "https://www.googleapis.com/civicinfo/v2/voterinfo"
         self._api_key = os.environ['GOOGLE_CIVIC_API_KEY']
+        self.date = dt.datetime.now().date()
+        self.client = CloudStorageClient()
+        self.path = "/tmp/"
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
     
     def fetch_voter_info(self, address, election_id=None):
         """
@@ -89,6 +97,80 @@ class VoterInfo():
         except requests.exceptions.RequestException as error:
             # Catastrophic error 
             logging.error(f"Fail: CivicInfo Api failed for {address}")
+            logging.error(error)
+            raise
+            
+    def save_voter_info(self, geoid, result, bucket_name):
+        """
+        Takes: 
+            - a geoid such as a county fips code or OCDid or other identifier as filename.
+            - the data returned for the geoid
+        Saves the file to the project bucket. 
+        """
+        blob_name = geoid + "_" + str(self.date) + '.json'
+        filepath = os.path.join(self.path, blob_name)
+        try: 
+            self.client.save_tmp_json(blob_name, result)
+            self.client.upload_file(filepath, bucket_name, blob_name)
+            logging.error(f"Successfully saved data for {geoid} to: gs://{bucket_name}/{blob_name}")
+        except Exception as error: 
+            logging.error(f"Error uploading data for {geoid} to gs://{bucket_name}/{bucket_name}.")
+            logging.error(error)
+            
+    def load_current_elections(self, bucket_name, blob_name): 
+        """
+        Load json file listing current elections. 
+        Validates 'elections' data in file
+        Takes: 
+        - bucket name 
+        - blob name of file 
+        
+        Returns: election data as json list
+        """ 
+        filepath = os.path.join(self.path, blob_name)
+        
+        #load data from Google Cloud Storage  
+        self.client.download_file(filepath, bucket_name, blob_name)
+        data = self.client.load_tmp_json(filepath)
+
+        try: 
+            elections = data['elections']
+            logging.info("Successfully loaded current elections data.")
+            return elections
+        except KeyError as error: 
+            logging.error(f"There are no current elections stored in file: 'gs://' {bucket_name} + '/' + {blob_name}")
+            raise
+        except Exception as error: 
+            logging.error(f"Error loading current elections from file: 'gs://' {bucket_name} + '/' + {blob_name}")
+            logging.error(error)
+            raise
+            
+    def load_address_locales(self, bucket_name, blob_name): 
+        filepath = os.path.join(self.path, blob_name)
+
+        #load temp file to gcp  
+        self.client.download_file(filepath, bucket_name, blob_name)
+
+        #load address data
+        try: 
+            with open(filepath, 'rb') as file:
+                data = pd.read_csv(filepath, encoding='utf-8')
+            logging.debug(f"Loaded address data from {filepath}")
+        except Exception as error: 
+            logging.error("Failed to load address data from /tmp/.")
+
+        try: 
+            #process data - ensure necessary columns available
+            addresses = data['address']
+            state_abbr = data['state_abbr']
+            data['fips'] = data['fips'].astype('str').str.zfill(5)
+            logging.info("Successfully loaded address lookup data.")
+            return data
+        except KeyError as error: 
+            logging.error(f"There are no current elections stored in file: 'gs://' {bucket_name} + '/' + {blob_name}")
+            raise
+        except Exception as error: 
+            logging.error(f"Error loading current elections from file: 'gs://' {bucket_name} + '/' + {blob_name}")
             logging.error(error)
             raise
             
