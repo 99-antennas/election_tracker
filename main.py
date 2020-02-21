a7d3f80f8ce01b759fc9bca78e5703d3ce2fb50e
kstohr <kstohr@users.noreply.github.com>
2020-02-21 00:00:13 -0400

    adds run_voter_info cloud function

--- main.py
+++ main.py
@@ -47,4 +47,75 @@ def run_current_elections(event, context):
 
     # Job status
     logging.info("Completed job fetch elections.")
+    
+    
+def run_voter_info(event, context): 
+    
+    """
+    Cloud function to run job to fetch voter information data for current elections
+    from Google Civic Information API.
+    Retrieves county level data and sets the fips code as the identifier (`geo_id`)
+    Stores data in a cloud storage bucket as a json file.
+    """
+    
+    # Job status
+    logging.info("Starting job fetch voter information")
+    logging.info("""Trigger: messageId {} published at {}""".format(context.event_id, context.timestamp))
+    
+    # Initiate job
+    civic = VoterInfo() 
+    
+    # Load elections, address data
+    logging.info("Load list of current elections.")
+    elections = civic.load_current_elections("current_elections",  "current_elections.json")
+    
+    logging.info("Load addreses by locale")
+    locales = civic.load_address_locales("address_locales",  "addresses_county.csv")
+    
+    # Retrieve Voter information data
+    for election in elections[0:2]:
+        election_id = election['id']
+        election_name = election['name']
+        election_ocdid = election['ocdDivisionId']
+        
+        # Get state abbr from OCDid
+        election_ocdid = election_ocdid.split("/")[-1].split(":")[-1].upper()
+
+        logging.debug(f"election_id: {election_id}")
+        logging.debug(f"election_ocdid: {election_ocdid}")
+        logging.debug(f"election_name: {election_name}")
+        
+        # Subset data by OCDid
+        # Except test election 
+        if election_name == 'VIP Test Election': 
+            continue
+        # If election is national, return data for all records
+        elif election_ocdid == 'US':
+            active = locales.copy()
+        # If election is statewide, return data for all records in state. 
+        else: 
+            active = locales.loc[locales['state_abbr'] == election_ocdid, :].copy()
+        # Ensure active elections not null
+        try: 
+            assert(active.empty == False)
+        except Exception as e: 
+            logging.error("Unable to subset data by OCDid.")
+    
+        # Get voter information for election
+        try: 
+            logging.info(f"Start election: {election_id}:{election_ocdid}") 
+            for index, row in active.iterrows():
+                address = row['address']
+                geo_id = row["fips"]
+                response = civic.fetch_voter_info(address, election_id)
+                response['geoid'] = {"fips":geo_id}
+                civic.save_voter_info(geo_id, response, bucket_name="current_contests")
+                time.sleep(1)
+            logging.debug(f"Completed election: {election_id}:{election_ocdid}")
+        except Exception as error: 
+            logging.error(f"Failed to retrieve data for {election_id}:{election_ocdid}")
+            logging.error(error)
+    
+    # Job status
+    logging.info("Completed job fetch voter info.")
 # End
