68fbe41fc3d4211bde2f054cbdba739e4b07879e
kstohr <kstohr@users.noreply.github.com>
2020-02-24 23:17:02 -0500

    deploys bucket with gsutils command

--- bin/deploy_run_current_elections.sh
+++ bin/deploy_run_current_elections.sh
@@ -5,6 +5,7 @@ source .env
 export GCP_FUNCTION_NAME=FetchElections
 export GCP_PUBSUB_TOPIC=CivicInfoAPI-elections
 export GCP_FUNCTION_ENTRY_POINT=run_current_elections
+export GCP_NEW_BUCKET=current_elections
 
 # Create requirements.txt
 # Note: syncs without re-locking and updating packages
@@ -13,15 +14,17 @@ pipenv run pip freeze > requirements.txt
 
 
 # Create Cloud buckets 
-# Note: If buckets exist this will generate an error - Ignore
-pipenv run python ./bin/create_storage_buckets.py
+# Note: If bucket exist this will generate an error - Ignore
+gsutil mb -p $GCP_PROJECT_NAME -c standard -l $GCP_COMPUTE_ZONE gs://$GCP_NEW_BUCKET
+
+# Set the retention policy for bucket objects
+# Not applicable.
 
 # Set Google Cloud project
 gcloud --quiet config set project $GCP_PROJECT_NAME
 
 # If not default, set region/zone
 gcloud --quiet config set compute/zone ${GCP_COMPUTE_ZONE}
-
 
 # Create PubSub topic
 # Note: If topic exist this will generate an error - Ignore
