3d20549bc40a26003ddf8f5fa05dd297e822e34e
kstohr <kstohr@users.noreply.github.com>
2020-02-24 21:52:49 -0500

    adds deployment script for `publish active elections`

---
+++ bin/deploy_pub_active_elections.sh
@@ -0,0 +1,42 @@
+### Script for deploying Cloud Function ###
+
+# Load environment variables
+source .env
+export GCP_FUNCTION_NAME=PubActiveElections
+export GCP_FUNCTION_ENTRY_POINT=publish_active_elections
+export TRIGGER_BUCKET_NAME=current_elections
+export GCP_PUBSUB_TOPIC=active_elections
+export GCP_NEW_BUCKET=active_elections
+
+# Create requirements.txt
+# Note: syncs without re-locking and updating packages
+pipenv sync
+pipenv run pip freeze > requirements.txt
+
+# Create Cloud bucket
+# Note: If bucket exist this will generate an error - Ignore
+# pipenv run python ./bin/create_storage_buckets.py
+gsutil mb -c standard -l $GCP_COMPUTE_ZONE gs://$GCP_NEW_BUCKET --retention 7d
+
+# Set Google Cloud project
+gcloud --quiet config set project $GCP_PROJECT_NAME
+
+# If not default, set region/zone
+gcloud --quiet config set compute/zone ${GCP_COMPUTE_ZONE}
+
+
+# Create PubSub topic
+# Note: If topic exist this will generate an error - Ignore
+gcloud pubsub topics create $GCP_PUBSUB_TOPIC
+
+# Deploy function 
+# https://cloud.google.com/sdk/gcloud/reference/functions/deploy
+# Add #--retry \ once tested
+gcloud functions deploy $GCP_FUNCTION_NAME \
+--source https://source.developers.google.com/projects/$GCP_PROJECT_NAME/repos/$GCP_REPOSITORY_ID \
+--runtime python37 \
+--trigger-bucket $TRIGGER_BUCKET_NAME \
+--entry-point $GCP_FUNCTION_ENTRY_POINT \
+--service-account api-requests@election-tracker-268319.iam.gserviceaccount.com \
+
+#End
