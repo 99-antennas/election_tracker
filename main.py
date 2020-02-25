1b7ca8fdc08cf8cf80f8348e92545512dd605f37
kstohr <kstohr@users.noreply.github.com>
2020-02-25 00:27:42 -0500

    fixes issues retrieving message attributes

--- main.py
+++ main.py
@@ -162,7 +162,8 @@ def publish_active_divisions(event, context):
     futures = dict()
     
     # Parse election 
-    election = event.message.attributes
+    logging.debug(f"{event}")
+    election = event['message']['attributes']
     election_id = election['election_id'] #renamed to avoid conflict
     election_name = election['name']
     election_ocdid = election['ocdDivisionId']
